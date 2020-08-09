6ff014fb4530cf2a92d69654cbefd7fe5d946159
Oleg Suslov <chibikstrong@gmail.com>
2020-08-09 14:10:22 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Greeting from './components/Greeting';
+import UserList from "./components/UserList";
+
+/*
+function App() {
+  return <Hello name={'React!'} />;
+}
+*/
+
+/*class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <UserList />;
+    }
+}*/
+
+const App = () => {
+    return <> <UserList /></>;
+}
+
+export default App;
